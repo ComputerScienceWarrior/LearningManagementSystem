a7d123c57b1f820b855a4cd2d1bb5a8e9f568760
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2022-04-22 22:22:21 -0700

    Create initial routes for User, Test and Course.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :users
+  resources :courses
+  resources :tests
+
+  resources :users do
+    resources :courses
+  end
+  
+  resources :users do
+    resources :tests
+  end
+
+  # root 'sessions#login'
 end
