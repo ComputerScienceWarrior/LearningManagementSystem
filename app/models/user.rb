d61bb7366bb21f3624370760ab7b19d7ee82338f
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2022-04-30 12:20:55 -0700

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
-    has_many_and_belongs_to :courses
+    has_and_belongs_to_many :courses
     has_many :tests
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
 
     extend FriendlyId
     friendly_id :full_name, use: :slugged
-
-    before do
-        full_name
-    end
 
     def full_name
         first_name + ' ' + last_name
