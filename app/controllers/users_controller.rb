a16da088257ffd41077bb08105c9131f84376100
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2022-05-21 10:27:54 -0700

    quick fix on broken code.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,7 @@ class UsersController < ApplicationController
 
     def update
         if @user.update(user_params)
+            @user.update(slug: @user.full_name.parameterize)
             redirect_to user_path(@user)
         else
             render :edit
